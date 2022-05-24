3a6a34b5526451ade5acdd4fb3cf5acf777767e2
tu6ge <772364230@qq.com>
2022-05-24 22:51:52 +0800

    wip: 比较新旧版本
    执行命令行

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use std::io::Read;
+
+use crates_io_api::{SyncClient, Error};
+use cargo_toml::Manifest;
+use version_compare::{Cmp, compare_to};
+use std::process::Command;
+
+fn main() {
+    //list_top_dependencies();
+
+    let published_version = get_published_version().unwrap();
+    let new_version = get_new_version().unwrap();
+
+    if compare_to(new_version, published_version, Cmp::Gt).unwrap() {
+        println!("新版本比较大");
+        let output = Command::new("git").arg("help").output().expect("git exec error!");
+        let output_str = String::from_utf8_lossy(&output.stdout);
+        println!("command res:{}", output_str)
+    }
+}
+
+
+fn get_published_version() -> Result<String, Error> {
+    // Instantiate the client.
+    let client = SyncClient::new(
+         "tu6ge (772364230@qq.com)",
+         std::time::Duration::from_millis(1000),
+    ).unwrap();
+    // Retrieve summary data.
+    let summary = client.get_crate("aliyun-oss-client")?;
+    Ok(summary.crate_data.max_version)
+}
+
+fn get_new_version() -> Option<String> {
+    let mut content: Vec<u8> = Vec::new();
+    std::fs::File::open("../oss/Cargo.toml")
+      .expect("open file failed").read_to_end(&mut content)
+      .expect("read_to_end failed");
+
+    let info = Manifest::from_slice(&content).expect("get version failed");
+
+    match info.package {
+        Some(v) => Some(v.version),
+        None => None
+    }
+}
