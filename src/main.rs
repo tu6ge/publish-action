3c17b5d28f4af7c54b4da07e86dedcfa1599cb47
tu6ge <tu6ge@qq.com>
2024-05-19 14:40:51 +0800

    feat: support multi project

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,22 @@
 //extern crate openssl;
 
-use std::{env, path::PathBuf};
-use std::{process::Command, task::Poll};
+use std::collections::HashSet;
+use std::fs::{read_to_string, File};
+use std::io::Read;
+use std::{env, path::Path};
+use std::process::Command;
+
+use serde::{Serialize, Deserialize};
 
 use crate::error::{Perror, Presult};
-use cargo::{
-    core::{Dependency, Registry, SourceId, Workspace},
-    sources::source::QueryKind,
-};
+
 #[cfg(test)]
 use dotenv::dotenv;
+use publish::publish;
 
 mod error;
 mod github;
-
-pub const CRATES_IO_REGISTRY: &str = "crates-io";
-
-#[derive(Debug)]
-enum PublicationStatus {
-    NotPublished,
-    Published,
-}
+mod publish;
 
 fn main() -> Presult<()> {
     //list_top_dependencies();
@@ -28,113 +24,73 @@ fn main() -> Presult<()> {
     #[cfg(test)]
     dotenv().ok();
 
-    let repository = env::var("GITHUB_REPOSITORY")?;
-    let branch = env::var("GITHUB_REF_NAME")?;
-    let token = env::var("GITHUB_TOKEN")?;
-    let path = env::var("GITHUB_WORKSPACE")?;
+    let config_str = read_to_string("./.github/publish.yml")?;
+    let config: ProjectList = serde_yaml::from_str(&config_str).unwrap();
 
-    let (name, version, publication_status) = get_publication_status(&path)?;
-    println!("repository: {}", repository);
-    println!("name: {}, version: {}", name, version);
-    println!("publication status: {:?}", publication_status);
+    println!("{:?}", config);
 
-    for pub_status in publication_status {
-        if matches!(pub_status, PublicationStatus::Published) {
-            //println!("::set-output name=new_version::false");
-            set_output("new_version=false");
-            println!("already published");
-            return Ok(());
-        }
+    if !config.check_same_error() {
+        panic!("this config have repeat projectes");
     }
 
-    //println!("::set-output name=new_version::true");
-    set_output("new_version=true");
-    println!("version not published");
-
-    let com_res = Command::new("cargo")
-        .arg("publish")
-        .current_dir(&path)
-        .status()?;
-    if !com_res.success() {
-        //println!("::set-output name=publish::false");
-        set_output("publish=false");
-        return Err(Perror::Input("publish command failed".to_string()));
+    for item in config.projects.into_iter() {
+        publish(item.dir, item.tag_prefix)?;
     }
-
-    let gh = github::Github::new(&repository, &token);
-    let sha = gh.get_sha(&branch)?;
-    println!("sha: {}", sha);
-
-    gh.set_ref(&version, &sha)?;
-    println!("new version {} is created", &version);
-    //println!("::set-output name=publish::true");
-    set_output("publish=true");
 
     Ok(())
 }
 
-type Name = String;
-type Version = String;
+#[derive(Debug, Serialize, Deserialize)]
+struct Project {
+    name: String,
+    dir: Option<String>,
+    tag_prefix: Option<String>,
+}
 
-fn get_publication_status(
-    workspace_root: &str,
-) -> Presult<(Name, Version, Vec<PublicationStatus>)> {
-    let mut config = cargo::util::Config::default()?;
+#[derive(Debug, Serialize, Deserialize)]
+struct ProjectList {
+    projects: Vec<Project>,
+}
 
-    config.configure(2, false, None, false, false, false, &None, &[], &[])?;
-    let mut cargo_toml = PathBuf::from(workspace_root);
-    cargo_toml.push("Cargo.toml");
-    cargo_toml = cargo_toml.canonicalize()?;
-    let workspace = Workspace::new(&cargo_toml, &config)?;
+impl ProjectList {
 
-    let package = workspace.current()?;
-    // Find where to publish
-    let publish_registries = package.publish();
-    let publish_registries = match publish_registries {
-        None => vec![CRATES_IO_REGISTRY.to_string()],
-        Some(v) => v.clone(),
-    };
-    if publish_registries.is_empty() {
-        return Err(Perror::PublishingDisabled);
-    }
-    let _lock = config.acquire_package_cache_lock()?;
-    // now - for each publication target, check whether it has this version (or newer)
-    let mut statuses = Vec::with_capacity(publish_registries.len());
-    for registry in publish_registries {
-        let source_id = if registry == CRATES_IO_REGISTRY {
-            SourceId::crates_io(&config)?
-        } else {
-            SourceId::alt_registry(&config, &registry)?
-        };
-        let mut package_registry = cargo::core::registry::PackageRegistry::new(&config)?;
-        package_registry.lock_patches();
-        let dep = Dependency::parse(
-            package.name(),
-            Some(&package.version().to_string()),
-            source_id,
-        )?;
-        let summaries = loop {
-            match package_registry.query_vec(&dep, QueryKind::Exact)? {
-                Poll::Ready(deps) => break deps,
-                Poll::Pending => package_registry.block_until_ready()?,
+    fn check_same_error(&self) -> bool {
+
+        let mut dirs = HashSet::new();
+        let mut prefixes = HashSet::new();
+        let mut default_dir_total = 0_u8;
+        let mut default_prefix_total = 0_u8;
+        for it in self.projects.iter() {
+            if let Some(path) = it.dir.clone() {
+                dirs.insert(path);
+            } else{
+                default_dir_total +=1;
             }
-        };
-        let matched = summaries
-            .iter()
-            .filter(|s| s.version() == package.version())
-            .count()
-            > 0;
-        statuses.push(if matched {
-            PublicationStatus::Published
-        } else {
-            PublicationStatus::NotPublished
-        });
+            if let Some(p) = it.tag_prefix.clone() {
+                prefixes.insert(p);
+            }else{
+                default_prefix_total +=1;
+            }
+            
+        }
+
+        if default_dir_total>1 {
+            return false
+        }
+        if default_prefix_total>1 {
+            return false;
+        }
+        if dirs.len() + (default_dir_total as usize) < self.projects.len() {
+            return false
+        }
+        if prefixes.len() + (default_prefix_total as usize) < self.projects.len() {
+            return false
+        }
+
+        true
+
+
     }
-    Ok((
-        package.name().to_string(),
-        package.version().to_string(),
-        statuses,
-    ))
 }
 
 fn set_output(info: &'static str) {
---
+++ src/publish.rs
@@ -0,0 +1,138 @@
+use std::{env, path::PathBuf, process::Command, task::Poll};
+
+use crate::{
+    error::{Perror, Presult},
+    github, set_output,
+};
+use cargo::{
+    core::{Dependency, Registry, SourceId, Workspace},
+    sources::source::QueryKind,
+};
+
+pub const CRATES_IO_REGISTRY: &str = "crates-io";
+
+#[derive(Debug)]
+pub(crate) enum PublicationStatus {
+    NotPublished,
+    Published,
+}
+
+pub(crate) fn publish(path: Option<String>, tag_prefix: Option<String>) -> Presult<()> {
+    let repository = env::var("GITHUB_REPOSITORY")?;
+    let branch = env::var("GITHUB_REF_NAME")?;
+    let token = env::var("GITHUB_TOKEN")?;
+    let mut gh_path = env::var("GITHUB_WORKSPACE")?;
+
+    if let Some(path) = path {
+      gh_path += &path;
+    }
+
+    let (name, version, publication_status) = get_publication_status(&gh_path)?;
+    println!("repository: {}", repository);
+    println!("name: {}, version: {}", name, version);
+    println!("publication status: {:?}", publication_status);
+
+    for pub_status in publication_status {
+        if matches!(pub_status, PublicationStatus::Published) {
+            //println!("::set-output name=new_version::false");
+            set_output("new_version=false");
+            println!("already published");
+            return Ok(());
+        }
+    }
+
+    //println!("::set-output name=new_version::true");
+    set_output("new_version=true");
+    println!("version not published");
+
+    let com_res = Command::new("cargo")
+        .arg("publish")
+        .current_dir(&gh_path)
+        .status()?;
+    if !com_res.success() {
+        //println!("::set-output name=publish::false");
+        set_output("publish=false");
+        return Err(Perror::Input("publish command failed".to_string()));
+    }
+
+    let gh = github::Github::new(&repository, &token);
+    let sha = gh.get_sha(&branch)?;
+    println!("sha: {}", sha);
+
+    let mut tag = match tag_prefix {
+      Some(p) => p, 
+      None => "".to_string(),
+    };
+    tag += &version;
+
+    gh.set_ref(&tag, &sha)?;
+    println!("new version tag {} is created", &tag);
+    //println!("::set-output name=publish::true");
+    set_output("publish=true");
+
+    Ok(())
+}
+
+type Name = String;
+type Version = String;
+
+fn get_publication_status(
+    workspace_root: &str,
+) -> Presult<(Name, Version, Vec<PublicationStatus>)> {
+    let mut config = cargo::util::Config::default()?;
+
+    config.configure(2, false, None, false, false, false, &None, &[], &[])?;
+    let mut cargo_toml = PathBuf::from(workspace_root);
+    cargo_toml.push("Cargo.toml");
+    cargo_toml = cargo_toml.canonicalize()?;
+    let workspace = Workspace::new(&cargo_toml, &config)?;
+
+    let package = workspace.current()?;
+    // Find where to publish
+    let publish_registries = package.publish();
+    let publish_registries = match publish_registries {
+        None => vec![CRATES_IO_REGISTRY.to_string()],
+        Some(v) => v.clone(),
+    };
+    if publish_registries.is_empty() {
+        return Err(Perror::PublishingDisabled);
+    }
+    let _lock = config.acquire_package_cache_lock()?;
+    // now - for each publication target, check whether it has this version (or newer)
+    let mut statuses = Vec::with_capacity(publish_registries.len());
+    for registry in publish_registries {
+        let source_id = if registry == CRATES_IO_REGISTRY {
+            SourceId::crates_io(&config)?
+        } else {
+            SourceId::alt_registry(&config, &registry)?
+        };
+        let mut package_registry = cargo::core::registry::PackageRegistry::new(&config)?;
+        package_registry.lock_patches();
+        let dep = Dependency::parse(
+            package.name(),
+            Some(&package.version().to_string()),
+            source_id,
+        )?;
+        let summaries = loop {
+            match package_registry.query_vec(&dep, QueryKind::Exact)? {
+                Poll::Ready(deps) => break deps,
+                Poll::Pending => package_registry.block_until_ready()?,
+            }
+        };
+        let matched = summaries
+            .iter()
+            .filter(|s| s.version() == package.version())
+            .count()
+            > 0;
+        statuses.push(if matched {
+            PublicationStatus::Published
+        } else {
+            PublicationStatus::NotPublished
+        });
+    }
+    Ok((
+        package.name().to_string(),
+        package.version().to_string(),
+        statuses,
+    ))
+}
