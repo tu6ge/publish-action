1695b9590df2c361697cc4137f60edc7336c9016
tu6ge <tu6ge@qq.com>
2024-05-20 10:04:54 +0800

    feat(action)

---
+++ index.js
@@ -0,0 +1,18 @@
+const core = require('@actions/core');
+const { spawn } = require('child_process');
+
+try {
+  const dir = core.getInput("dir");
+  const tag_prefix = core.getInput("tag_prefix");
+  const publish = spawn('publish-action', ['-d', dir, '-t', tag_prefix]);
+
+  publish.stdout.on('data', (data)=> {
+    core.setOutput(data);
+  })
+  publish.stderr.on('data', (data)=> {
+    core.setFailed(data);
+  });
+
+} catch(e) {
+  core.setFailed(e.message);
+}
