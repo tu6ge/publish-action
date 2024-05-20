5d53260c1c5b59444848f882cf7308b69019ade1
tu6ge <tu6ge@qq.com>
2024-05-20 11:29:08 +0800

    debug

--- dist/index.js
+++ dist/index.js
@@ -26820,7 +26820,7 @@ const { exec } = __nccwpck_require__(2081);
 try {
   const dir = core.getInput("dir");
   const tag_prefix = core.getInput("tag_prefix");
-  exec(`publish-action -d ${dir} -t ${tag_prefix}`, (error, stdout, stderr) => {
+  exec(`publish-action`, ['-d', dir, '-t', tag_prefix], (error, stdout, stderr) => {
     core.setOutput(stdout);
     core.setFailed(stderr);
 
