cc0ed29c4a037cb2a1e03b8dc3013d23052705e2
tu6ge <tu6ge@qq.com>
2024-05-20 11:16:30 +0800

    debug

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 const core = require('@actions/core');
-const { spawn } = require('child_process');
+const { exec } = require('child_process');
 
 try {
   const dir = core.getInput("dir");
   const tag_prefix = core.getInput("tag_prefix");
-  const publish = spawn('publish-action', ['-d', dir, '-t', tag_prefix]);
+  exec(`publish-action -d ${dir} -t ${tag_prefix}`, (error, stdout, stderr) => {
+    core.setOutput(stdout);
+    core.setFailed(stderr);
 
-  publish.stdout.on('data', (data)=> {
-    core.setOutput(data);
-  })
-  publish.stderr.on('data', (data)=> {
-    core.console.error(data);
-    core.console.error(data.data);
-    core.setFailed(data);
   });
+
+  // publish.stdout.on('data', (data)=> {
+  //   core.setOutput(data);
+  // })
+  // publish.stderr.on('data', (data)=> {
+  //   core.console.error(data);
+  //   core.console.error(data.data);
+  //   core.setFailed(data);
+  // });
 
 } catch(e) {
   core.setFailed(e.message);
