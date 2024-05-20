fe7c36dbfadc9fea5ab221c6cb946b7e6cb18662
tu6ge <tu6ge@qq.com>
2024-05-20 11:29:08 +0800

    debug

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { exec } = require('child_process');
 try {
   const dir = core.getInput("dir");
   const tag_prefix = core.getInput("tag_prefix");
-  exec(`publish-action -d ${dir} -t ${tag_prefix}`, (error, stdout, stderr) => {
+  exec(`publish-action`, ['-d', dir, '-t', tag_prefix], (error, stdout, stderr) => {
     core.setOutput(stdout);
     core.setFailed(stderr);
 
